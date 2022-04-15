f224e0870ebcee9b94ba93286b6525bfbd030b75
Asifur Rahman <asifur2014@hotmail.com>
2022-04-15 02:00:32 -0400

    Initialized Default Authentication for ASP.NET

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FinancialPlanner.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FinancialPlannerDBContextConnection");;
+
+builder.Services.AddDbContext<FinancialPlannerDBContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<FinancialPlannerDBContext>();;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +27,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
